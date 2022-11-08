628e05a9b2ea854c8f074953d5ff9074093e56bf
MdRashid62 <rashy.mr62@gmail.com>
2022-11-08 16:25:35 +0500

    template-driven form final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Template Driven Forms';
-
-  
+  countryList: country[] = [
+    new country("1", "India"),
+    new country("2", "USA"),
+    new country("2", "England")
+  ];
+  onSubmit(contactForm: { value: any; }) {
+    console.log(contactForm.value);
+  }
 }
+
+export class country {
+  id:string;
+  name:string;
+
+  constructor(id:string, name:string) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms'; //import FormMdodules
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule  //Add in Imports Array
   ],
   providers: [],
   bootstrap: [AppComponent]
